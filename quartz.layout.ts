46276a9fbac77b1c93078d50ab009d14f3ace6ff
dralagen <dralagen@dralagen.fr>
2024-05-17 09:22:06 +0000

    fix footer url with the source of notes

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,7 +7,7 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/dralagen",
+      GitHub: "https://github.com/dralagen/notes",
     },
   }),
 }
