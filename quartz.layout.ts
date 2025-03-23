3226b37493bcbdf0275a8eb5d7d4978c07aed59b
Dralagen <dralagen@dralagen.fr>
2025-03-23 22:42:40 +0100

    🔧 update footer link to use public target repository

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/dralagen/notes",
+      GitHub: "https://github.com/dralagen/notes.dralagen.fr",
     },
   }),
 }
