300647b4140f9f0a5310367621da735c99284630
dralagen <dralagen@dralagen.fr>
2025-04-24 17:01:13 +0200

    feat(UI): modify font

--- quartz.config.ts
+++ quartz.config.ts
@@ -29,9 +29,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Open Sans",
+        body: "Open Sans",
+        code: "JetBrains Mono",
       },
       colors: {
         lightMode: {
